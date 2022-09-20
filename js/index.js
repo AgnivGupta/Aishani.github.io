ca0d030d10fd4c882af61e19e0bc727caf1ae331
AgnivGupta <agnivgupta176@gmail.com>
2022-09-20 21:48:55 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -294,10 +294,9 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/yKNxeF4KMsY?controls=0&loop=1&autoplay=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/yKNxeF4KMsY?autoplay=1&mute=1&enablejsap=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
   document.querySelector('#video').appendChild(ifrm);
 }
-
