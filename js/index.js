7980171fcc67388501f34f051738afbe54231cbe
AgnivGupta <agnivgupta176@gmail.com>
2022-09-20 21:53:03 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/yKNxeF4KMsY?autoplay=1&mute=1&enablejsap=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/yKNxeF4KMsY?autoplay=1&enablejsap=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
